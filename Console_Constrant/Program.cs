8a353168fdf0403b2af282fc13acf472edf85005
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-08 23:36:06 -0400

    Add project files.

---
+++ Console_Constrant/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Constrant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Area.Circle());
+            Console.ReadKey();
+        }
+        
+    }    
+
+    public class Area
+    {
+        private const double PI = 3.14;
+        public static double Circle()
+        {
+         Console.Write("Enter Radius of the Circle:");
+         int Radius = int.Parse(Console.ReadLine());
+         double area = (PI * Radius);           
+         return area;
+        }
+    }
+}
